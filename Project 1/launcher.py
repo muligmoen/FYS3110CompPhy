ebe4848e51583351165ba051c3f7f971fea825db
Magnus Ulimoen <flymagnus@gmail.com>
2015-09-08 11:58:19 +0200

    Added python launcher

---
+++ Project 1/launcher.py
@@ -0,0 +1,38 @@
+#!/usr/bin/ev python3
+
+import subprocess
+
+def get_state_size():
+  N = 0
+  print('Please state the size of the system (>=2)')
+  while N<2:
+    N = int(input('N = '))
+  return str(N)
+
+def get_filename():
+  print('Please state the filename to save the file to, default=test.txt')
+  filename = input('Filename = ')
+  if filename == '':
+    filename = 'test.txt'
+  return filename
+
+def get_algtype():
+  print('Please choose the algorithm to use, M - matrix decomp,\n' +
+      'S - sparse matrix decomp, T - Thomas algorithm,\n' +
+      'default=T')
+  alg = input('<type> = ')
+  if alg not in ['S','M','T']:
+    alg = 'T'
+  return alg
+  
+
+N = get_state_size()
+
+filename = get_filename()
+alg_type = get_algtype()
+
+command = ['./project1', N, filename, alg_type]
+subprocess.call(command)
+
+command = ['./plot.py', filename]
+subprocess.call(command)
