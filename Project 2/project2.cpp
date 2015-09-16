5920d75aa270d563aaf8b228013457d4e8250038
Magnus Ulimoen <flymagnus@gmail.com>
2015-09-16 14:03:19 +0200

    Improve rotate algorithm

--- Project 2/project2.cpp
+++ Project 2/project2.cpp
@@ -8,17 +8,20 @@
 
 void rotate(arma::Mat<double> &A, double c, double s, int k, int l)
 {
+  double a_kk = A(k, k);
+  double a_ll = A(l, l);
+  
   for (int iii=0; iii<(int)A.n_rows; iii++)
   {
     if (iii != k && iii != l)
     {
-      A(iii,k) = A(iii, k)*c - A(iii, l)*s;
+      A(iii, k) = A(iii, k)*c - A(iii, l)*s;
       A(iii, l) = A(iii, l)*c + A(iii, l)*s;
     }
   }
   A(k, k) = A(k, k)*c*c - 2*A(k,l)*c*s + A(l, l)*s*s;
   A(l, l) = A(l, l)*c*c + 2*A(l,k)*c*s + A(k, k)*s*s;
-  A(k,l) = 0;
+  A(k,l) = (a_kk-a_ll)*c*s + A(k,l)*(c*c-s*s);
 }
 
 void test_rotate()
@@ -27,22 +30,20 @@ void test_rotate()
   A << 1 << 5 << arma::endr
     << 0 << 3;
     
-
-    
   double tau = 1/5.;
   double t = -tau - std::sqrt(1+ 1/(tau*tau));
   double c = 1/std::sqrt(1+t*t);
   double s = t*c;
-  std::cout << c << " " << s << std::endl;
+  
   
   rotate(A, c, s, 0, 1);
   
-  arma::Mat<double> B;
-  B << c*c-5*s*c+3*s*s << 5*c*c-2*s*c << arma::endr
-    << -5*s*s-2*s*c << s*s+5*s*c+3*c*c;
+  arma::Mat<double> S;
+  S << c << s << arma::endr
+    << -s << c;
     
+  std::cout << S.t()*A*S << std::endl;
   std::cout << A << std::endl;
-  std::cout << B << std::endl;
 }
 
 void max_offdiag_indexes(const arma::Mat<double> &A, int &k, int &l)
@@ -76,7 +77,7 @@ void test_max_indexes()
 
 int main()
 {
-  //test_rotate();
+  test_rotate();
   /*arma::Mat<double> A;
   A << 1 << 5 << 6 << arma::endr
     << 0 << 3 << 7;
