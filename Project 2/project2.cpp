14dc7c3e5bc1994aaa1f38d6a101b4ca846d27d5
krikarls <krikarls@math.uio.no>
2015-09-16 12:06:38 +0200

    Function for finding position of largest matrix element added

--- Project 2/project2.cpp
+++ Project 2/project2.cpp
@@ -7,6 +7,22 @@ inline T abs(T num)
   return num ? num > 0 : -num;
 }
 
+void largest_value(const arma::mat &A, int &k, int &l)
+{
+double val = 0;
+  for (int ii=0; ii < A.n_cols; ii++)
+  {
+     for (int jj=0; jj < A.n_cols; jj++)
+        {
+          if((ii != jj) && (std::abs(A(ii,jj))) > val) {
+            val = std::abs(A(ii,jj));
+            k=ii;
+            l=jj;
+            }
+         
+        }
+  }
+}
 
 void rotate(arma::Mat<double> &A, double c, double s, int k, int l)
 {
