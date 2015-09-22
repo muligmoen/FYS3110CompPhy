9a9e2cf70bd3ae9d34bd1544b46b459935591e97
Magnus Ulimoen <flymagnus@gmail.com>
2015-09-22 22:37:50 +0200

    Rename function to ham_matrix(), take rho_inf from cli, make new section in report.tex

--- Project 2/project2.cpp
+++ Project 2/project2.cpp
@@ -8,7 +8,9 @@
 
 
 
-arma::Mat<double> get_matrix(int N, double rho_0, double rho_inf);
+arma::Mat<double> ham_matrix(int N, double rho_0, double rho_inf, double (*V)(double));
+
+inline double potential(double rho);
 
 
 int main(int argc, char *argv[])
@@ -16,21 +18,22 @@ int main(int argc, char *argv[])
   UnitTest::RunAllTests();
   
   int N = -1;
-  int MAX_iter = -1;
+  int rho_inf;
   
   if (argc < 3)
   {
     N = 100;
-    MAX_iter = 10000;
+    rho_inf = 10;
+    
     std::cout << "Running standard settings" << std::endl;
     std::cout << "N = " << N << std::endl;
-    std::cout << "Max iterations = " << MAX_iter << std::endl;
+    std::cout << "rho_inf = " << rho_inf << std::endl;
   }
   else 
   {
     N = std::atoi(argv[1]);
-    MAX_iter = std::atof(argv[2]);
-    if (N<1 || MAX_iter<1)
+    rho_inf = std::atof(argv[2]);
+    if (N<1 || rho_inf<=0)
     {
       std::cerr << "Invalid arguments, exiting" << std::endl;
       std::exit(1);
@@ -38,53 +41,37 @@ int main(int argc, char *argv[])
   }
 
   double rho_0 = 0.0;
-  double rho_inf = 10;
   
+  arma::Mat<double> A = ham_matrix(N, rho_0, rho_inf, potential);
   
-  arma::Mat<double> A = get_matrix(N, rho_0, rho_inf);
+  double max_err = 100;
   
-
-  auto eigs =  eig_sym(A);
-
- 
-  for (int iii=0; iii<MAX_iter; iii++)
+  while (max_err > 1e-10)
   {
     int k, l;
-    double max_err;
     max_err_offdiag(A, k, l, max_err);
-    
-    
-    if (max_err < 1e-10)
-    {
-      break;
-    }
     
     double cos, sin;
     find_cos_sin(A(k,k), A(l,l), A(k,l), cos, sin);
     
     rotate(A, cos, sin, k, l);
-
   }
   
-  arma::Col<double> eigs2(N);
- 
-  for (int iii=0; iii<N; iii++)
-  {
-    eigs2(iii) = A(iii,iii);
-  }
-  eigs2 = sort(eigs2);
+  arma::vec eigs = A.diag();
   
+  arma::vec eig = sort(eigs);
   
   for (int iii=0; iii<std::min(10, N); iii++)
   {
-    std::cout << eigs(iii) << " " << eigs2(iii) << "\n";
+    std::cout << eig(iii) << std::endl;
   }
+  
   return 0;
 }
 
 
 
-arma::Mat<double> get_matrix(int N, double rho_0, double rho_inf)
+arma::Mat<double> ham_matrix(int N, double rho_0, double rho_inf, double (*V)(double))
 {
   arma::Mat<double> A(N, N, arma::fill::zeros);
   
@@ -96,8 +83,13 @@ arma::Mat<double> get_matrix(int N, double rho_0, double rho_inf)
   for (int iii=0; iii<N; iii++)
   {
     double rho = rho_0 + iii*h;
-    A(iii,iii) = 2*inv_h_square + rho*rho;
+    A(iii,iii) = 2*inv_h_square + V(rho);
   }
   
   return A;
+}
+
+inline double potential(double rho)
+{
+  return rho*rho;
 }
