1e7db35bb5172a2d69c3c07eaaaaa717b2327e6c
krikarls <krikarls@math.uio.no>
2015-11-11 10:39:01 +0100

    added program for finite scaling

---
+++ Project 4/finite_scaling_LSq.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.array([20,40,60,80])		# L values
+y=np.array([-1,0.2,0.9,2.1])	# corresponding approximated T_c values
+
+A = np.vstack([x, np.ones(len(x))]).T 	# setting up matrix system for least squares method
+a, c = np.linalg.lstsq(A, y)[0]			# solving the system, obtaining the slope
+
+plt.plot(x, y, 'o', label='$T_c (L)$', markersize=10)
+plt.plot(x, a*x + c, 'r', label='Best fit')
+plt.title('Fitting line through $T_c(L)$ to obtain the slope $a=$ {}'.format(a))
+plt.legend()
+plt.show()
