61ee1336e756b46204420ada1017614fcba3c470
Magnus Ulimoen <flymagnus@gmail.com>
2015-12-10 23:18:27 +0100

    Saving to file, parallel MC methods

--- Project 5/figures.py
+++ Project 5/figures.py
@@ -5,9 +5,13 @@ import os
 
 saveloc = os.path.join('report', 'pics')
 
-def get_stdout(cmd):
-  p = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
-  return p.stdout.read()
+def run_command(cmd):
+  p = subprocess.Popen(cmd)
+  p.wait()
+
+def get_lines():
+  with open('test.txt') as f:
+    return f.readlines()
 
 def plot_stuff(u_ANA, u_eulerF, u_eulerB, u_CN, u_MC, u_MCG):
   fig, ax = plt.subplots()
@@ -19,7 +23,7 @@ def plot_stuff(u_ANA, u_eulerF, u_eulerB, u_CN, u_MC, u_MCG):
   plt.plot(x, u_eulerB, linewidth=2, label='Euler backward')
   plt.plot(x, u_CN, linewidth=2, label='Crank Nicolson')
   plt.plot(x, u_MC, linewidth=2, label='Monte Carlo')
-  plt.plot(x, u_MCG, linewidth=2, label='Monte Carlo variable timestep')
+  plt.plot(x, u_MCG, linewidth=2, label='Monte Carlo variable')
 
   plt.title('Diffusion of particles',fontsize=16)
   plt.xlabel('x')
@@ -32,33 +36,35 @@ def plot_stuff(u_ANA, u_eulerF, u_eulerB, u_CN, u_MC, u_MCG):
 
   plt.axis([-0.2, 1.2, 0, 1])
 
-  plt.legend(loc='upper right')
-  savename = os.path.join(saveloc, 'dx={},dt={}.png'.format(dx, dt))
+  plt.legend(loc='best')
+  savename = os.path.join(saveloc, 'dx={}_t={}.png'.format(dx, T))
   plt.savefig(savename, dpi=400, bbox_inches='tight')
   plt.show()
   
   
 alpha = 0.5 # Stability criteria
-dx = 1/10
+dx = 1/100
 dt = dx*dx*alpha
-T = 0.01
+T = 0.2
 
 cmd = ['./project5', str(dt), str(dx), str(T)]
 
-output = get_stdout(cmd)
+run_command(cmd)
+lines = get_lines()
 
-lines = output.split('\n')
 
-u_ANA = list(map(float, lines[0].strip('[]').split()))
-u_eulerF = list(map(float, lines[1].strip('[]').split()))
-u_eulerB = list(map(float, lines[3].strip('[]').split()))
-u_CN = list(map(float, lines[5].strip('[]').split()))
-u_MC = list(map(float, lines[7].strip('[]').split()))
-u_MCG = list(map(float, lines[10].strip('[]').split()))
 
-MC_variance = list(map(float, lines[9].strip('[]').split()))
-MCG_variance = list(map(float, lines[12].strip('[]').split()))
+u_ANA = list(map(float, lines[7][1:-2].split()))
+u_eulerF = list(map(float, lines[9][1:-2].split()))
+u_eulerB = list(map(float, lines[12][1:-2].split()))
+u_CN = list(map(float, lines[15][1:-2].split()))
+u_MC = list(map(float, lines[18][1:-2].split()))
+u_MCG = list(map(float, lines[23][1:-2].split()))
 
-E = [float(lines[2]), float(lines[4]), float(lines[6]), float(lines[8]), float(lines[11])]
+MC_variance = list(map(float, lines[7][1:-2].split()))
+MCG_variance = list(map(float, lines[7][1:-2].split()))
+
+E = [float(lines[10][0]), float(lines[13][0]), float(lines[16][0]),
+     float(lines[19][0]), float(lines[24][0])]
 
 plot_stuff(u_ANA, u_eulerF, u_eulerB, u_CN, u_MC, u_MCG)
--- Project 5/src/main.cpp
+++ Project 5/src/main.cpp
@@ -6,6 +6,9 @@
 #include <chrono>
 #include <cstdlib>
 #include <cmath>
+#include <fstream>
+
+#include <omp.h>
 
 using namespace diffusion;
 
@@ -31,45 +34,66 @@ int main(int argc, char **argv)
   
   // MC thingies
   const int Nsteps = 2*N*N*T; // MC timesteps
-  const int Nparticles = 1000;
+  const int Nparticles = 1000*N; // Number of particles per bin*# of bins
   const int MC_repetitions = 10;
-  
   const auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
   
+  
+  omp_set_num_threads(4);
+  
+  std::ofstream file;
+  file.open("test.txt");
+  
+  file << "Run of the program for diffusion\n"
+       << Delta_t << " Delta t\n"
+       << Delta_x << " Delta x\n"
+       << T << " Time\n"
+       << Nparticles << " Number of particles MC\n"
+       << seed << " Seed used for MC\n";
+       
+  
+ 
   //Analytical solution
   auto v_ANA = Analytical(T, N);
   auto u_ANA = u_steady + v_ANA;
-  std::cout << u_ANA << "\n";
+  file << "Analytical solution\n"
+       << u_ANA << "\n";
   
   
   // Forward Euler
   auto v_EF = forward_euler(-u_steady, alpha, Ntimesteps);
   auto u_EF = u_steady + v_EF;
-  std::cout << u_EF << "\n" << Error(u_EF, u_ANA) << "\n";
+  file << "Forward Euler\n"
+       << u_EF << "\n" << Error(u_EF, u_ANA) << " Error\n";
   
   // Backward Euler
   auto v_EB = backward_euler(-u_steady, alpha, Ntimesteps);
   auto u_EB = u_steady + v_EB;
-  std::cout << u_EB << "\n" << Error(u_EB, u_ANA) << "\n";
+  file << "Backward Euler\n"
+       << u_EB << "\n" << Error(u_EB, u_ANA) << " Error\n";
   
   // Crank Nicolson
   auto v_CN = Crank_Nicolson(-u_steady, alpha, Ntimesteps);
   auto u_CN = u_steady + v_CN;
-  std::cout << u_CN << "\n" << Error(u_CN, u_ANA) << "\n";
+  file << "Crank Nicolson\n"
+       << u_CN << "\n" << Error(u_CN, u_ANA) << " Error\n";
   
   
   //Monte Carlo equal steplength
   Vector<int> sum_MC(N, [](){return 0;});
   Vector<int> quadrate_sum_MC(N, [](){return 0;});
-    
+   
+  #pragma omp parallel for
   for (int ii=0; ii<MC_repetitions; ii++){
     const auto vec = Monte_Carlo(N, Nsteps, Nparticles, seed+ii);
-    sum_MC += vec;
-    quadrate_sum_MC += vec*vec;
+    #pragma omp critical
+    {
+      sum_MC += vec;
+      quadrate_sum_MC += vec*vec;
+    }
   }
   
   auto u_MC = normalise(sum_MC, Nparticles*MC_repetitions);
-  std::cout << u_MC << "\n" << Error(u_MC, u_ANA) << "\n";
   
   Vector<double> variance(N);
   auto u_MCsquare = normalise(quadrate_sum_MC, Nparticles*Nparticles*MC_repetitions);
@@ -78,7 +102,10 @@ int main(int argc, char **argv)
     variance[ii] = std::sqrt(u_MCsquare[ii] - u_MC[ii]*u_MC[ii]);
   }
     
-  std::cout << variance << "\n";
+  file << "Monte Carlo equal steplength\n"
+       << u_MC << "\n" << Error(u_MC, u_ANA) << " Error\n"
+       << "variance\n"
+       << variance << "\n";
   
   // Monte Carlo gaussian
   const double L0_factor = 2; // how much Delta_x is shrinked (changes number of timesteps)
@@ -88,15 +115,18 @@ int main(int argc, char **argv)
   Vector<int> sum_MCG(N, [](){return 0;});
   Vector<int> quadrate_sum_MCG(N, [](){return 0;});
     
+  #pragma omp parallel for
   for (int ii=0; ii<MC_repetitions; ii++){
     const auto vec = Monte_Carlo_gaussian(Nsteps*L0_factor*L0_factor, N, Nparticles, L0, seed+ii);
-    sum_MCG += vec;
-    quadrate_sum_MCG += vec*vec;
+    #pragma omp critical
+    {
+      sum_MCG += vec;
+      quadrate_sum_MCG += vec*vec;
+    }
   }
   
   const double norm_factor = (double)sum_MCG[0]/(double)MC_repetitions;
   auto u_MCG = normalise(sum_MCG, norm_factor*MC_repetitions);
-  std::cout << u_MCG << "\n" << Error(u_MCG, u_ANA) << "\n";
   
   auto u_MCGsquare = normalise(quadrate_sum_MCG, norm_factor*norm_factor*MC_repetitions);
   
@@ -105,6 +135,9 @@ int main(int argc, char **argv)
     varianceG[ii] = std::sqrt(u_MCGsquare[ii] - u_MCG[ii]*u_MCG[ii]);
   }
   
-  std::cout << varianceG << "\n";
-  
+  file << "Monte Carlo variable steplength\n"
+       << u_MCG << "\n" << Error(u_MCG, u_ANA) << " Error\n"
+       << "Variance\n" << varianceG << "\n";
+ 
+  file.close();
 }
